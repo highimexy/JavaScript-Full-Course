47c2a3bf3cd47c948c4bdcb146346de2b29a0fc4
highimexy <teslawiktor@gmail.com>
2025-09-29 20:37:01 +0200

    8:10 while = loop

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -3,7 +3,14 @@ const todoList = [];
 function addTodo() {
  const inputElement = document.querySelector('.js-input');
  const name = inputElement.value;
- console.log(name);
+
+
+ todoList.push(name);
+ console.log(todoList);
+
+//epmty imput
+inputElement.value = '';
+
 }
 
 // od jutra 2 godzinny minimum
