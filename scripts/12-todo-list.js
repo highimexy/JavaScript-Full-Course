88b9b3714b1543a291e24c8cf8cf30173cdaaae9
highimexy <teslawiktor@gmail.com>
2025-10-01 21:36:47 +0200

    11:08

---
+++ scripts/12-todo-list.js
@@ -0,0 +1,49 @@
+
+const todoList = [];
+
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  todoList.forEach(function(todoObject, index){
+    const { name, dueDate } = todoObject;
+    const html = `
+      <div class="todo-grid">
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button onclick="
+          todoList.splice(${index}, 1);
+          renderTodoList();
+        " class="delete-todo-button">Delete</button>
+      </div>
+    `;
+
+    todoListHTML += html;
+  });
+  
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-date-input');
+  const dueDate = dateInputElement.value;
+
+if (name !== '' && dueDate !== '') {
+  todoList.push({
+    name,
+    dueDate
+  });
+
+  inputElement.value = '';
+  dateInputElement.value = '';
+  renderTodoList();
+}
+
+}
