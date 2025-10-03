ec660724e122847abda2af352a1e17569d079951
highimexy <teslawiktor@gmail.com>
2025-10-03 11:25:25 +0200

    10:46

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -13,20 +13,29 @@ function renderTodoList() {
       <div class="todo-grid">
         <div>${name}</div>
         <div>${dueDate}</div>
-        <button onclick="
-          todoList.splice(${index}, 1);
-          renderTodoList();
-        " class="delete-todo-button">Delete</button>
+        <button  class="delete-todo-button js-delete-todo-button">Delete</button>
       </div>
     `;
 
     todoListHTML += html;
   });
-  
+
 
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index , 1);
+        renderTodoList();
+      });
+    });
 }
+
+document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+  addTodo();
+});
 
 function addTodo() {
   const inputElement = document.querySelector('.js-input');
@@ -35,15 +44,15 @@ function addTodo() {
   const dateInputElement = document.querySelector('.js-date-input');
   const dueDate = dateInputElement.value;
 
-if (name !== '' && dueDate !== '') {
-  todoList.push({
-    name,
-    dueDate
-  });
+  if (name !== '' && dueDate !== '') {
+    todoList.push({
+      name,
+      dueDate
+    });
 
-  inputElement.value = '';
-  dateInputElement.value = '';
-  renderTodoList();
-}
+    inputElement.value = '';
+    dateInputElement.value = '';
+    renderTodoList();
+  }
 
 }
