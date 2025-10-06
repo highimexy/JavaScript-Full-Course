90e3a709a4118c04969188284fe9fd67f450db41
highimexy <teslawiktor@gmail.com>
2025-10-06 15:58:51 +0200

    13:17 Generate HTML for order summary

--- Amazon project/javascript-amazon-project/data/cart.js
+++ Amazon project/javascript-amazon-project/data/cart.js
@@ -1,4 +1,12 @@
-export const cart = [];
+export const cart = [{
+  productId: '54e0eccd-8f36-462b-b68a-8182611d9add',
+  quantity: 2,
+}, {
+  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1,
+}
+
+];
 
 export function addToCart(productId) {
   let matchingItem;
