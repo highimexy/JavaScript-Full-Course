487bcd60b2944e4d3f6636f6b36fb712823a4c0a
highimexy <teslawiktor@gmail.com>
2025-10-13 17:03:03 +0200

    15:50 2 types of test cases // Basic and Edge

--- Amazon project/javascript-amazon-project/tests/moneyTest.js
+++ Amazon project/javascript-amazon-project/tests/moneyTest.js
@@ -6,7 +6,13 @@ if (formatCurrency(2095) === '20.95') {
     console.log('failed');
 };
 
-if(formatCurrency(0) === '0.00') {
+if (formatCurrency(0) === '0.00') {
+    console.log('passed');
+} else {
+    console.log('failed');
+};
+
+if (formatCurrency(2000.5) === '20.01') {
     console.log('passed');
 } else {
     console.log('failed');
