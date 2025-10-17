b97860cdd413955a41c92e99216229886dc528d8
highimexy <teslawiktor@gmail.com>
2025-10-17 13:49:18 +0200

    17:00

--- Amazon project/javascript-amazon-project/tests-jasmine/checkout/orderSummaryTest.js
+++ Amazon project/javascript-amazon-project/tests-jasmine/checkout/orderSummaryTest.js
@@ -1,9 +1,26 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
+import { loadFromStorage } from "../../data/cart.js";
 
-describe('test suite: renderOrderSummary', () => {
-    it('displays the cart', () => {
-        document.querySelector('.js-test-container').innerHTML = `
+describe("test suite: renderOrderSummary", () => {
+  it("displays the cart", () => {
+    document.querySelector(".js-test-container").innerHTML = `
             <div class="js-order-summary"></div>
         `;
+
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: "54e0eccd-8f36-462b-b68a-8182611d9add",
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+        {
+          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+          quantity: 1,
+          deliveryOptionId: "2",
+        },
+      ]);
     });
+    loadFromStorage();
+  });
 });
