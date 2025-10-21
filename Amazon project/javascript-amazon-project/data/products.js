d4352084c94296248a7e0606118410ffb77511dc
highimexy <teslawiktor@gmail.com>
2025-10-21 20:44:26 +0200

    19:11

--- Amazon project/javascript-amazon-project/data/products.js
+++ Amazon project/javascript-amazon-project/data/products.js
@@ -34,6 +34,10 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return ``;
+  }
 }
 
 class Clothing extends Product {
@@ -42,6 +46,13 @@ class Clothing extends Product {
   constructor(productDetails) {
     super(productDetails);
     this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+    <a href="${this.sizeChartLink}" target="_blank">
+    Size chart</a>
+    `;
   }
 }
 
@@ -531,5 +542,9 @@ export const products = [
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
 ].map((productDetails) => {
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);
+  }
+
   return new Product(productDetails);
 });
