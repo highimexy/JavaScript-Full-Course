b4b7e454e03135906941af7b2a68be8852b0f8b8
highimexy <teslawiktor@gmail.com>
2025-10-22 12:51:01 +0200

    19:22

--- Amazon project/javascript-amazon-project/data/products.js
+++ Amazon project/javascript-amazon-project/data/products.js
@@ -76,6 +76,15 @@ console.log(date);
 console.log(date.toLocaleTimeString());
 */
 
+/*
+console.log(this);
+
+const object2 = {
+  a: 2,
+  b: this.a
+};
+*/
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
