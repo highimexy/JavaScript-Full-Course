6c8354d8b6296b037450d1a024fe07376d38c829
highimexy <teslawiktor@gmail.com>
2025-10-22 12:48:17 +0200

    19:19

--- Amazon project/javascript-amazon-project/data/products.js
+++ Amazon project/javascript-amazon-project/data/products.js
@@ -70,6 +70,12 @@ const tshirt = new Clothing({
   sizeChartLink: "images/clothing-size-chart.png",
 });
 
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
