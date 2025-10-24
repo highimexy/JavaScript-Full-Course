49ee68367665dafcbf4c32b82cb823119eed5d00
highimexy <teslawiktor@gmail.com>
2025-10-24 10:42:33 +0200

    21:13 Conver loadProducts from XHR to fetch

--- Amazon project/javascript-amazon-project/data/products.js
+++ Amazon project/javascript-amazon-project/data/products.js
@@ -102,6 +102,27 @@ const object3 = {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch("https://supersimplebackend.dev/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((producsData) => {
+      products = producsData.map((productDetails) => {
+        if (productDetails.type === "clothing") {
+          return new Clothing(productDetails);
+        }
+
+        return new Product(productDetails);
+      });
+    });
+  return promise;
+}
+/*
+loadProductsFetch().then(() =>{
+  console.log('next step');
+});
+*/
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
